e00f0e9887b1b9090418f226fc35be0d5ebaa193
Marco Kuis <marco.kuis@gmail.com>
2016-03-21 21:30:24 +0100

    Trying again

--- rp-server.js
+++ rp-server.js
@@ -4,14 +4,14 @@ var app = express();
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 
-mongoose.connect('mongodb://rpadmin:rolewithit@ds013579.mlab.com:13579/rpdb');       //verbinding maken met mongo database rpdb. LET OP MONGO SERVER MOET DRAAIEN OP LOCALHOST
+mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://rpadmin:rolewithit@ds013579.mlab.com:13579/rpdb');       //verbinding maken met mongo database rpdb. LET OP MONGO SERVER MOET DRAAIEN OP LOCALHOST
 app.use(bodyParser.urlencoded({ 'extended': 'true' }));            // parse application/x-www-form-urlencoded
 app.use(bodyParser.json());                                     // parse application/json
 
 app.use(express.static('public'));
 
 //app luistert naar requests op port 13579
-app.listen(13579, function () {
+app.listen(process.env.PORT || 13579, function () {
     console.log('todo app on port 13579');
 });
 
