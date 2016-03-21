70fc8de5c96eb63037e54c26c4c3ab4a17197de5
Marco.Kuis <marco.kuis@gmail.com>
2016-03-21 16:01:36 +0100

    Verdere vormgeving. Mongo server naar mLab gelinkt.

--- rp-server.js
+++ rp-server.js
@@ -4,15 +4,15 @@ var app = express();
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 
-mongoose.connect('mongodb://localhost/rpdb');       //verbinding maken met mongo database rpdb. LET OP MONGO SERVER MOET DRAAIEN OP LOCALHOST
+mongoose.connect('mongodb://rpadmin:rolewithit@ds013579.mlab.com:13579/rpdb');       //verbinding maken met mongo database rpdb. LET OP MONGO SERVER MOET DRAAIEN OP LOCALHOST
 app.use(bodyParser.urlencoded({ 'extended': 'true' }));            // parse application/x-www-form-urlencoded
 app.use(bodyParser.json());                                     // parse application/json
 
 app.use(express.static('public'));
 
-//app luistert naar requests op port 3000
-app.listen(3000, function () {
-    console.log('todo app on port 3000');
+//app luistert naar requests op port 13579
+app.listen(13579, function () {
+    console.log('todo app on port 13579');
 });
 
 require('./app/gameDataApi')(app);
